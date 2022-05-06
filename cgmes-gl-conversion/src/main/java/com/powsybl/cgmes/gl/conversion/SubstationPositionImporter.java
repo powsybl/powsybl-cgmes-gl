38ac49b0dea8281d70aa1c845b94536ade03abc3
Jon Harper <jon.harper87@gmail.com>
2022-05-06 16:52:41 +0200

    Use iidm-api instead of iidm-impl to create extensions (#44)
    Signed-off-by: HARPER Jon <jon.harper87@gmail.com>

--- cgmes-gl-conversion/src/main/java/com/powsybl/cgmes/gl/conversion/SubstationPositionImporter.java
+++ cgmes-gl-conversion/src/main/java/com/powsybl/cgmes/gl/conversion/SubstationPositionImporter.java
@@ -11,8 +11,7 @@ import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.Substation;
 import com.powsybl.triplestore.api.PropertyBag;
 import com.powsybl.iidm.network.extensions.Coordinate;
-import com.powsybl.iidm.network.extensions.SubstationPosition;
-import com.powsybl.iidm.network.impl.extensions.SubstationPositionImpl;
+import com.powsybl.iidm.network.extensions.SubstationPositionAdder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,10 +39,10 @@ public class SubstationPositionImporter {
         String substationId = substationPositionData.getId("powerSystemResource");
         Substation substation = network.getSubstation(substationId);
         if (substation != null) {
-            SubstationPosition substationPosition = new SubstationPositionImpl(substation,
-                    new Coordinate(substationPositionData.asDouble("y"), substationPositionData.asDouble("x")));
             // y <=> lat, x <=> lon
-            substation.addExtension(SubstationPosition.class, substationPosition);
+            substation.newExtension(SubstationPositionAdder.class).withCoordinate(
+                new Coordinate(substationPositionData.asDouble("y"), substationPositionData.asDouble("x"))
+            ).add();
         } else {
             LOG.warn("Cannot find substation {}, name {} in network {}: skipping substation position", substationId, substationPositionData.get("name"), network.getId());
         }
